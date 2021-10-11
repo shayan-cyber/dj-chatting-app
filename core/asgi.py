337ca28b14134346ffcf297c230579e937896307
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-10-12 02:52:27 +0530

    test dploy 6

--- core/asgi.py
+++ core/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from channels.routing import URLRouter, ProtocolTypeRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
@@ -17,7 +17,7 @@ from .routing import ws_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     'websocket':AllowedHostsOriginValidator(
         AuthMiddlewareStack(

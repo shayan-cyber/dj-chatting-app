0604b4925905952abf500b5b35d5bc8ba8ad7908
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-10-12 08:53:39 +0530

    test deploy 11

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: daphne core.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=core.settings -v2 
--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,7 @@ import dj_database_url
 import django_heroku
 
 from pathlib import Path
-import django
-django.setup()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

38df55e416e0ac2fdf27a19615feca7ae4e8773c
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-10-12 09:37:47 +0530

    test deploy 13

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 import dj_database_url
 import django_heroku
 
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'channels',
-    'chat_app'
+    
+    
 ]
 
 MIDDLEWARE = [

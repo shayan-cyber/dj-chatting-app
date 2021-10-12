22ee27ff5c99ce0ead884656e78f946d787f1850
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-10-12 09:15:08 +0530

    test deploy 12

--- core/settings.py
+++ core/settings.py
@@ -154,3 +154,6 @@ DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
